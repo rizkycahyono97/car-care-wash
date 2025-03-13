745af62b4d4c012c299cf95b551672803fb74337
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-03-13 14:54:41 +0700

    integrasi halaman home

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\CarService;
+use App\Models\City;
 
 class FrontController extends Controller
 {
     //
+    public function index()
+    {
+        $cities = City::all(); // get all cities
+        $services = CarService::withCount(['storeServices'])->get(); // get all services with count of store services
+
+        return view('front.index', compact('cities', 'services'));
+    }
 }
