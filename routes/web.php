f37a475e89c6100aa33ca833323f1de349aa7849
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-03-13 12:52:19 +0700

    persiapan Routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,37 @@
 <?php
 
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Frontend Routes
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+
+Route::get('/transactions', [FrontController::class, 'transactions'])->name('front.transactions');
+
+Route::get('/transactions/details', [FrontController::class, 'transaction_details'])->name('front.transactions.details');
+
+Route::get('/search', [FrontController::class, 'search'])->name('front.search');
+
+Route::get('/store/details/{carStore:slug}', [FrontController::class, 'details'])->name('front.details');
+
+Route::get('/booking/payment/submit', [FrontController::class, 'booking_payment_store'])->name('front.booking.payment.store');
+
+Route::get('/booking/{carStore.slug}', [FrontController::class, 'booking'])->name('front.booking');
+
+Route::get('/booking/{carStore:slug}/{carService:slug}', [FrontController::class, 'booking_store'])->name('front.booking.store');
+
+Route::get('/booking/{carStore}/{carService}/payment', [FrontController::class, 'booking_payment'])->name('front.booking.payment');
+
+Route::get('/booking/success/{bookingTransaction}', [FrontController::class, 'success_booking'])->name('front.success.booking');
+
+
+
+
+
+
+
+
